257e3a761362cea0f4654b2310cc44e0c1ff3b55
Varela <ivarela@186590d00b81.ant.amazon.com>
2017-11-14 14:51:05 -0800

    Demo of ruby syntax

--- widget.rb
+++ widget.rb
@@ -3,6 +3,19 @@ def ask(question)
   gets.chomp
 end
 
+def price(quant)
+    if quant >= 100
+        price_per_item = 8
+    elsif quant >= 50
+        price_per_item = 9
+    else
+        price_per_item =  10
+    end
+    quant * price_per_item
+end
+
 puts "Welcome to the widget store!"
 answer = ask("How many widgets would you like to order?")
-puts "You entered #{answer} widgets"
+number = answer.to_i
+total = price(number)
+puts "For #{answer} widgets, your total will be #{total}"
